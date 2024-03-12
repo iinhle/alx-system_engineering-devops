6c64b3179e8e52a4dc928b5073d3136dd715233a
iinhle <sawcyzee@gmail.com>
2024-03-11 17:28:45 -0700

    updated

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,30 +1,30 @@
-# Setup New Ubuntu server with nginx
-# and add a custom HTTP header
-
-exec { 'update system':
-        command => '/usr/bin/apt-get update',
-}
+#!/usr/bin/env bash
+# dOCS
 
 package { 'nginx':
-	ensure => 'installed',
-	require => Exec['update system']
+  ensure => installed,
 }
 
-file {'/var/www/html/index.html':
-	content => 'Hello World!'
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^\/redirect_me https:\/\/github.com\/iinhle permanent;"
-	provider => 'shell'
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
 }
 
-exec {'HTTP header':
-	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
-	provider => 'shell'
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
 
-service {'nginx':
-	ensure => running,
-	require => Package['nginx']
+file_line { 'Creating header':
+  	ensure => present,
+  	path   => '/etc/nginx/sites-available/default',
+  	line   => "\tadd_header X-Served-By ${hostname};",
+  	after  => 'server_name _;',
+	require => Service['nginx'],
 }
