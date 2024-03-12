52596b23fcc6ff850cb13cb03ca1eda529adfa48
iinhle <sawcyzee@gmail.com>
2024-03-11 17:50:35 -0700

    Sky is the limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,4 @@
-# Change the OS configuration so that it is possible to login with the
+# Changing the OS configuration so that it is possible to login with the
 # holberton user and open a file without any error message.
 
 exec {'OS security config':
